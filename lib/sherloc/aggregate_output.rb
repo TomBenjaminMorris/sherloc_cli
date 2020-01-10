9229950d768572cd84c04fea28e18185f5035c9f
Tom Morris <thomasmorris823@gmail.com>
2020-01-10 14:24:02 +0000

    Aggregate output logic

--- lib/sherloc/aggregate_output.rb
+++ lib/sherloc/aggregate_output.rb
@@ -10,10 +10,12 @@ module Sherloc::AggregateOutput
         data = JSON.parse(json_file)
 
         vulerabilityArray = Array.new
+        highVulerabilityArray = Array.new
 
         data.each_with_index do | item, index |
             item['Vulnerabilities'].each_with_index do | innerItem, innerIndex |
                 vulerabilityArray << { vulerabilityID: innerItem['VulnerabilityID'], target: item['Target'], pkgName: innerItem['PkgName'], severity: innerItem['Severity'] }
+                highVulerabilityArray << { vulerabilityID: innerItem['VulnerabilityID'], description: innerItem['Description'] } if innerItem['Severity'] == 'HIGH'
             end
         end
 
@@ -27,14 +29,27 @@ module Sherloc::AggregateOutput
 
         vulerability_count.sort_by {|_key, value| value}
 
+        puts "\n-------------------------------------------------------- REPORT --------------------------------------------------------"
+
         # puts vulerability_count
         puts Rainbow("\nVulnerability Severity\n").underline
         puts Rainbow(" HIGH: ").red.inverse + " " + severity_count['HIGH'].to_s + " " + Rainbow(" MEDIUM: ").yellow.inverse + " " + severity_count['MEDIUM'].to_s + " " + Rainbow(" LOW: ").green.inverse + " " + severity_count['LOW'].to_s
 
-        puts Rainbow("\nVulnerability IDs\n").underline
+        puts Rainbow("\nVulnerability ID Count\n").underline
         vulerability_count.keys.each do | item |
-            puts item + " => " + vulerability_count[item].to_s
+            puts item + " => " + Rainbow(" " + vulerability_count[item].to_s + " ").indianred.inverse
         end
+
+        # Description of high severities if any
+        if highVulerabilityArray != []
+            puts Rainbow("\nHIGH Vulnerability Descriptions\n").underline
+
+            highVulerabilityArray.each_with_index do | item, index |
+                puts "\n" + (index + 1).to_s + ") " + Rainbow(" " + item[:vulerabilityID].to_s + " ").cyan.inverse + " - " + item[:description].to_s
+            end
+        end
+
+        puts "\n--------------------------------------------------------- END ---------------------------------------------------------"
 
     end
 end
