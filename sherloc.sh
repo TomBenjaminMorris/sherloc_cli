77ee4108033350e43847a1d01db7d9e60dbfc229
Tom Morris <thomasmorris823@gmail.com>
2020-01-15 15:54:27 +0000

    update sherloc exe file

--- sherloc.sh
+++ sherloc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #docker run -v /var/run/docker.sock:/var/run/docker.sock -it docker.artifactory.euw.platformservices.io/docker/morrist/sherloc:v0.1 $1 $2 $3
-docker run -v /var/run/docker.sock:/var/run/docker.sock -it sherloc $1 $2 $3
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v $PWD/test_image_file:/test_image_file -it sherloc $1 $2 $3
