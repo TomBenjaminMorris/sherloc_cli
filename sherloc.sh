9b9febee2c69b2138441968b9fd029b10c455ad7
Tom Morris <thomasmorris823@gmail.com>
2020-01-15 15:30:52 +0000

    slim dockerfile and update sherloc exe file

--- sherloc.sh
+++ sherloc.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker run -v /var/run/docker.sock:/var/run/docker.sock -it sherloc:v0.1 $1 $2 $3
+#docker run -v /var/run/docker.sock:/var/run/docker.sock -it docker.artifactory.euw.platformservices.io/docker/morrist/sherloc:v0.1 $1 $2 $3
+docker run -v /var/run/docker.sock:/var/run/docker.sock -it sherloc $1 $2 $3
