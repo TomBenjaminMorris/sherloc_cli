0de5c8a1583786a4797c1f8c1a96caa2f52802ad
Tom Morris <thomasmorris823@gmail.com>
2020-01-19 09:48:04 +0000

    Add unit tests and clean up code

--- lib/sherloc/parse_multi.rb
+++ lib/sherloc/parse_multi.rb
@@ -3,30 +3,21 @@ require 'rainbow'
 
 module Sherloc::ParseMulti
 
-    def self.run!(options, args)
-
-        # Separate arguments and switches
-
-        scan_file = args[0]
-        isMulti = options[:m]
-        isCodeFile = options[:f]
-        isImage = options[:i]
+    def self.run!(scan_file, isImage, isCodeFile)
         
         file_ext = File.extname(scan_file)
         isJS = file_ext == ".js"
 
-        if isMulti
-            puts Rainbow("Attempting to parse multi file...\n").cyan
-            f_lines = File.open(scan_file).read.split("\n")
-    
-            f_lines.each_with_index do |line, index|
-    
-                file_ext = File.extname(line)
-                isJS = file_ext == ".js"
+        puts Rainbow("Attempting to parse multi file...\n").cyan
+        f_lines = File.open(scan_file).read.split("\n")
 
-                if (isCodeFile && !isJS) || (isImage && isJS)
-                    raise Rainbow("Unsupported file type in multi file\n").red.inverse
-                end
+        f_lines.each_with_index do |line, index|
+
+            file_ext = File.extname(line)
+            isJS = file_ext == ".js"
+
+            if (isCodeFile && !isJS) || (isImage && isJS)
+                raise Rainbow("Unsupported file type in multi file\n").red.inverse
             end
         end
 
--- test/sherloc_test.rb
+++ test/sherloc_test.rb
@@ -12,45 +12,91 @@ class DefaultTest < Test::Unit::TestCase
         @options_image_multi = {"f"=>false, :f=>false, "file"=>false, :file=>false, "m"=>true, :m=>true, "multi-scan"=>true, :"multi-scan"=>true, "i"=>true, :i=>true, "image"=>true, :image=>true}
     end
 
-    def test_that_it_has_a_version_number
-        refute_nil ::Sherloc::VERSION
-    end
+    # def test_that_it_has_a_version_number
+    #     refute_nil ::Sherloc::VERSION
+    # end
 
-    # detect.rb
+    # # detect.rb
 
-    def test_detect_file_positive
-        isJS, ext = Sherloc::Detect.run!(@scan_file_js)
-        assert isJS
-    end
+    # def test_detect_file_positive
+    #     isJS, ext = Sherloc::Detect.run!(@scan_file_js)
+    #     assert isJS
+    # end
 
-    def test_detect_file_negative
-        isJS, ext = Sherloc::Detect.run!(@scan_file_java)
-        assert !isJS
-    end
+    # def test_detect_file_negative
+    #     isJS, ext = Sherloc::Detect.run!(@scan_file_java)
+    #     assert !isJS
+    # end
 
-    # validate.rb
+    # # validate.rb
 
-    def test_validation_js_file
-        isMulti, isCode, isImage = Sherloc::Validate.run!(@options_file_single, @scan_file_js)
-        assert isMulti == false && isCode == true && isImage == false
-    end
+    # def test_validation_js_file
+    #     isMulti, isCode, isImage = Sherloc::Validate.run!(@options_file_single, @scan_file_js)
+    #     assert isMulti == false && isCode == true && isImage == false
+    # end
 
-    def test_validation_js_file_negative
-        assert_raise RuntimeError do
-            isMulti, isCode, isImage = Sherloc::Validate.run!(@options_file_single, @scan_file_java)
+    # def test_validation_js_file_negative
+    #     assert_raise RuntimeError do
+    #         isMulti, isCode, isImage = Sherloc::Validate.run!(@options_file_single, @scan_file_java)
+    #     end
+    # end
+
+    # def test_validation_js_image_negative
+    #     assert_raise RuntimeError do
+    #         isMulti, isCode, isImage = Sherloc::Validate.run!(@options_image_single, @scan_file_js)
+    #     end
+    # end
+
+    # def test_validation_js_image
+    #     isMulti, isCode, isImage = Sherloc::Validate.run!(@options_image_single, @scan_file_image)
+    #     assert isMulti == false && isCode == false && isImage == true
+    # end
+
+    # # Scan Image single
+
+    # def test_single_image_scan
+    #     Sherloc::ScanImage.run!(@scan_file_image)
+    #     file_path = './json_files/results.json'
+    #     json_file = File.open(file_path).read
+    #     assert json_file != nil
+    # end
+
+    # def test_single_image_scan_negative
+    #     assert_raise 'Errno::ENOENT(<No such file or directory @ rb_sysopen - ./bad_path/results.json>)' do
+    #         Sherloc::ScanImage.run!(@scan_file_image)
+    #         file_path = './bad_path/results.json'
+    #         json_file = File.open(file_path).read
+    #     end
+    # end
+
+    # Parse Multi File
+
+    def test_parse_multi_image_file
+        File.open "./test_image_file_test", 'w' do |file| 
+            file.write "alpine\nalpine"
+            file.close
         end
+        f_lines = Sherloc::ParseMulti.run!('./test_image_file_test', true, false)
+        assert f_lines != nil
+        File.delete("./test_image_file_test")
     end
 
-    def test_validation_js_image_negative
+    def test_parse_multi_image_file_with_js
         assert_raise RuntimeError do
-            isMulti, isCode, isImage = Sherloc::Validate.run!(@options_image_single, @scan_file_js)
+            File.open "./test_image_file_test", 'w' do |file| 
+                file.write "alpine.js\nalpine.js"
+                file.close
+            end
+            f_lines = Sherloc::ParseMulti.run!('./test_image_file_test', true, false)
+            assert f_lines != nil
         end
+        File.delete("./test_image_file_test")
     end
-
-    def test_validation_js_image
-        isMulti, isCode, isImage = Sherloc::Validate.run!(@options_image_single, @scan_file_image)
-        assert isMulti == false && isCode == false && isImage == true
-    end
-
-
+    
+    
+    
+    
+    
+    
+    
 end
