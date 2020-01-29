03f2672bc7f935cd616726b27fc12899fa03eab3
Tom Morris <thomasmorris823@gmail.com>
2020-01-29 14:57:30 +0000

    Update exe script with parameter validation

--- sherloc.sh
+++ sherloc.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-#docker run -v /var/run/docker.sock:/var/run/docker.sock -it docker.artifactory.euw.platformservices.io/docker/morrist/sherloc:v0.1 $1 $2 $3
-docker run -v /var/run/docker.sock:/var/run/docker.sock -v $PWD/test_image_file:/test_image_file -it sherloc-slim:v0.1 $1 $2 $3
+
+if [[ "$#" -ne 3 && "$1" != "help" ]]
+then
+    echo "Incorrect number of parapeters passed into script"
+    echo "Correct format './sherloc scan <option> <argument>'"
+    exit 1
+fi
+
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v $PWD/image_file:/image_file -it docker.artifactory.euw.platformservices.io/docker/morrist/sherloc:v0.2 $1 $2 $3
+
